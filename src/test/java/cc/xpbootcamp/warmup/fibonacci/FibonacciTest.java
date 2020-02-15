50c4dfa8a82509ee60ccbf15b73d4fbc7e52c2a9
“wjincheng” <2411465039@qq.com>
2020-02-16 00:20:52 +0800

    give 1 return 1

---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -0,0 +1,26 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.Test;
+
+//import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.Assert.assertEquals;
+
+
+public class FibonacciTest {
+    @Test
+    public void should_return_1_when_calculate_given_position_is_1() {
+        //given
+        int position = 1;
+
+        //when
+        Fibonacci fibonacci = new Fibonacci();
+        int result = fibonacci.calculate(1);
+
+        //then
+        assertEquals(result, 1);
+
+    }
+
+}
