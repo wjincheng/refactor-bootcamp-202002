e730965ebf65eef9450c34ae52707e586b03a978
“wjincheng” <2411465039@qq.com>
2020-02-16 00:28:40 +0800

    give 4 return 3

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -7,6 +7,9 @@ public class Fibonacci {
         if (position <= init) {
             return 1;
         }
-        return 2;
+        if (position == 3) {
+            return 2;
+        }
+        return 3;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -48,4 +48,18 @@ public class FibonacciTest {
 
     }
 
+    @Test
+    public void should_return_4_when_calculate_given_position_is_3() {
+        //given
+        int position = 4;
+
+        //when
+        Fibonacci fibonacci = new Fibonacci();
+        int result = fibonacci.calculate(position);
+
+        //then
+        assertEquals(result, 3);
+
+    }
+
 }
