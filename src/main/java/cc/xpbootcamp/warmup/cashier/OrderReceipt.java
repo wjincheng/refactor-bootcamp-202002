a1a1178aa5ec8472b5e85e4edb726d8e5119c8e7
“wjincheng” <2411465039@qq.com>
2020-02-24 09:21:44 +0800

    edit commit

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -8,10 +8,11 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
+ * OrderReceipt prints the details of order including customer description, quantity,
  * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
+ * of order. And it has a 0.02 discount every Wednesday.
+ * It computes the total order amount (amount of individual lineItems +
+ * total sales tax - discount) and prints it.
  *
  */
 public class OrderReceipt {
